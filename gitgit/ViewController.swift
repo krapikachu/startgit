e59c719aadcec989d129e8fcfb8f3da7a5230fb3
Reum <krapika@gmail.com>
2017-09-18 21:43:31 +0900

    hhhh

---
+++ gitgit/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  gitgit
+//
+//  Created by Reum on 2017. 9. 18..
+//  Copyright © 2017년 Reum. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        //냐냐냐냐냐냐냐냐냐냐
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
